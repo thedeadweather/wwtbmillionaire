09715e5c4652f1bdf707f9413823296277351269
Alex Kln <kalininmalinin@gmail.com>
2020-07-10 18:02:00 +0300

    edit user#show tests

--- spec/views/user/show.html.erb_spec.rb
+++ spec/views/user/show.html.erb_spec.rb
@@ -2,18 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
 
+  # любой пользователь
   context 'for any user' do
     before(:each) do
       assign(:user, FactoryBot.build_stubbed(:user, name: 'Тестович'))
       stub_template 'users/_game.html.erb' => 'User game goes here'
+
       render
     end
 
-    # пользователь видит там свое имя
+    # видит там свое имя
     it 'renders player name' do
       expect(rendered).to match 'Тестович'
     end
 
+    # не видит кнопку редактирования
     it 'shouldnt render edit button' do
       expect(rendered).not_to match 'Сменить имя и пароль'
     end
@@ -25,17 +28,31 @@ RSpec.describe 'users/show', type: :view do
     end
   end
 
-  # текущий пользователь (и только он) видит кнопку для смены пароля
+  # залогиненный пользователь
   context "for current user" do
-    let(:user) { FactoryBot.build_stubbed(:user) }
+    let(:user) { FactoryBot.build_stubbed(:user, name: 'Тестович') }
     let(:current_user) { assign(:user, user) }
     before do
       allow(view).to receive(:current_user).and_return(current_user)
+      stub_template 'users/_game.html.erb' => 'User game goes here'
+
       render
     end
 
+    # пользователь видит там свое имя
+    it 'renders player name' do
+      expect(rendered).to match 'Тестович'
+    end
+
+    # только он видит кнопку для смены пароля
     it 'should render edit button' do
       expect(rendered).to match 'Сменить имя и пароль'
+    end
+
+    # на странице отрисовываются фрагменты с игрой
+    it 'renders game' do
+	    render partial: 'users/game'
+	    expect(rendered).to match 'User game goes here'
     end
   end
 end
